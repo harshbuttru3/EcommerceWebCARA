e694766749429f311b55aa55cb6f5dcf79dc280f
Harsh <shivamkumarpaswan7488700578@gmail.com>
2023-09-27 21:24:01 +0530

    fix-issue:fixed the plus and minus button to update number of items in cart by making two seperate functions to increase and decrease the items in cart

--- cart.js
+++ cart.js
@@ -24,39 +24,77 @@ function renderCartItems(){
                     <small>$</small>${item.price}
                 </div>
                 <div class="units">
-                    <div class="btn minus" onclick="changeNumberOfUnits('minus', ${JSON.stringify(item)})">-</div>
+                    <div class="btn minus" onclick="decreaseNumberOfUnits('${item.id}')">-</div>
                     <div class="number">${item.numberOfUnits}</div>
-                    <div class="btn plus" onclick="changeNumberOfUnits('plus', ${JSON.stringify(item)})">+</div>           
+                    <div class="btn plus" onclick="increaseNumberOfUnits('${item.id}')">+</div>
+          
                 </div>
             </div>
         `
     })
 }
-
-function changeNumberOfUnits(action, item){
+// Function to increase the number of units for an item in the cart
+function increaseNumberOfUnits(itemId) {
     cart = cart.map((cartItem) => {
-        console.log(cartItem.id,item.id);
-        // if(cartItem.id === item.id){
-            
-        //     let numberOfUnits = cartItem.numberOfUnits;
-        //     if(action === "minus" && numberOfUnits > 1){
-        //         numberOfUnits--;
-        //     } else if(action === "plus"){
-        //         numberOfUnits++;
-        //     }
-        //     return {
-        //         ...cartItem,
-        //         numberOfUnits,
-        //     };
-        // } 
-        // else {
-        //     return cartItem;
-        // }
+        if (cartItem.id === itemId) {
+            return {
+                ...cartItem,
+                numberOfUnits: cartItem.numberOfUnits + 1,
+            };
+        } else {
+            return cartItem;
+        }
     });
-    
-    updateCart();
+
+    // Update the cart in localStorage after modifying it
     localStorage.setItem('CART', JSON.stringify(cart));
+    updateCart();
 }
+
+// Function to decrease the number of units for an item in the cart
+function decreaseNumberOfUnits(itemId) {
+    cart = cart.map((cartItem) => {
+        if (cartItem.id === itemId && cartItem.numberOfUnits > 1) {
+            return {
+                ...cartItem,
+                numberOfUnits: cartItem.numberOfUnits - 1,
+            };
+        } else {
+            return cartItem;
+        }
+    });
+
+    // Update the cart in localStorage after modifying it
+    localStorage.setItem('CART', JSON.stringify(cart));
+    updateCart();
+}
+
+
+// function changeNumberOfUnits(action, item) {
+//     console.log("Hello world");
+//     console.log("item.id:", item.id);
+//     cart = cart.map((cartItem) => {
+//         console.log("cartItem.id:", cartItem.id);
+//         if (cartItem.id === item.id) {
+//             let numberOfUnits = cartItem.numberOfUnits;
+//             if (action === "minus" && numberOfUnits > 1) {
+//                 numberOfUnits--;
+//             } else if (action === "plus") {
+//                 numberOfUnits++;
+//             }
+//             return {
+//                 ...cartItem,
+//                 numberOfUnits,
+//             };
+//         } else {
+//             return cartItem;
+//         }
+//     });
+
+//     // Update the cart in localStorage after modifying it
+//     localStorage.setItem('CART', JSON.stringify(cart));
+//     updateCart();
+// }
 
 // Calculate and render subtotal
 function renderSubTotal(){
